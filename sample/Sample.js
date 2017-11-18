c7813fa83babc2687a1eb8db719a74d91707faed
wandalen <wandalen.me@gmail.com>
2017-11-18 16:38:17 +0200

    adjust sample

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,8 +1,14 @@
 
-
 if( typeof module !== 'undefined' )
-require( 'wTools' );
+{
+  require( 'wTools' );
+  require( 'wfieldsstack' );
+}
+
 var _ = wTools;
 
-/**/
+console.log( 'wFieldsStack mixin included' );
 
+/*
+!!! write sample of mixin it in
+*/
