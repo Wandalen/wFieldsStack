f04de10edd3d9ac75c1f48fd446cf39341d942af
Wandalen <wandalen.me@gmail.com>
2018-06-07 10:14:32 +0300

    sync

--- staging/dwtools/abase/mixin/FieldsStack.s
+++ staging/dwtools/abase/mixin/FieldsStack.s
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var _ = _global_.wTools;
+var _global = _global_; var _ = _global_.wTools;
 var _hasOwnProperty = Object.hasOwnProperty;
 
 if( typeof module !== 'undefined' )
@@ -14,7 +14,7 @@ if( typeof module !== 'undefined' )
     let toolsExternal = 0;
     try
     {
-      require.resolve( toolsPath );
+      toolsPath = require.resolve( toolsPath );/*hhh*/
     }
     catch( err )
     {
@@ -25,7 +25,7 @@ if( typeof module !== 'undefined' )
     require( toolsPath );
   }
 
-  var _ = _global_.wTools;
+  var _global = _global_; var _ = _global_.wTools;
 
   _.include( 'wProto' );
 
@@ -187,7 +187,7 @@ _global_[ Self.name ] = _[ Self.nameShort ] = _.mixinMake( Self );
 // --
 
 if( typeof module !== 'undefined' )
-if( _global_._UsingWtoolsPrivately_ )
+if( _global_.WTOOLS_PRIVATE )
 delete require.cache[ module.id ];
 
 if( typeof module !== 'undefined' && module !== null )
